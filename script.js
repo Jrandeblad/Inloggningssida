2c37e6269d7c607f685e5c8b277048bf2d99bb59
Jonas Randeblad <fjunas@hotmail.com>
2022-09-30 20:41:12 +0200

    Slutgiltiga versionen!

--- script.js
+++ script.js
@@ -1,61 +1,87 @@
+// Sätter värden att kolla inloggningen mot.
 const correctUserName = "Sara";
 const correctPassword = "qwe123";
-const loginButton = document.getElementById("loginButton");
-
-function redanInloggad() {
-    if (localStorage.getItem("userName") === correctUserName && localStorage.getItem("password") === correctPassword) {
-        loggaIn();
-    }    else {
-        document.getElementById("felLogIn").style.display = "none";
-        document.getElementById("logIn").style.display = "flex";
-
-    }
-
-}
-
-function loggaIn() {
-    document.getElementById("logIn").style.display = "none";
-    document.getElementById("header").style.display = "flex";
-    var styleElem = document.body.appendChild(document.createElement("style"));
-    styleElem.innerHTML = "#background::before {opacity: 1}";   
-    document.getElementById("simhallen").innerText += ` - Välkommen in ${localStorage.getItem("userName")}!`;
-    document.getElementById("felLogIn").style.display = "none";
-}
-
-function loggaUt() {
-    localStorage.clear();
+// Byter vilka element som ska visas på login-sidan.
+function switchToLoginPage() {
+    document.getElementById("tryAgain").style.display = "none";
     document.getElementById("logIn").style.display = "flex";
     document.getElementById("header").style.display = "none";
-    var styleElem = document.body.appendChild(document.createElement("style"));
-    styleElem.innerHTML = "#background::before {opacity: 0.85}";
-    document.getElementById("simhallen").innerText = "Simhallens hemsida"
-    document.getElementById("felLogIn").style.display = "none";
 }
-
-redanInloggad();
-
-loginButton.onclick = function() {
-    let userName = document.getElementById("userName").value;
-    let password = document.getElementById("password").value;
-
-    if (userName === correctUserName && password === correctPassword) {
-        localStorage.setItem("userName", userName);
-        localStorage.setItem("password", password);
-        loggaIn();
-       // document. = `Välkommen till simhallens sida ${userName}`
+// Byter vilka element som ska visas på sidan när det blev fel användarnamn/lösenord.
+function switchToTryAgainPage() {
+    document.getElementById("tryAgain").style.display = "flex";
+    document.getElementById("logIn").style.display = "none";
+    document.getElementById("header").style.display = "none";
+}
+// Byter vilka element som ska visas på sidan vid korrekt inloggning.
+function switchToMainPage() {
+    document.getElementById("tryAgain").style.display = "none";
+    document.getElementById("logIn").style.display = "none";
+    document.getElementById("header").style.display = "flex";
+}
+// Skapar ett style-element och ändrar det för att ändra bakgrundens genomskinlighetsgrad.
+function changeOpacity(x) {
+    var styleElem = document.body.appendChild(document.createElement("style"));
+    styleElem.innerHTML = `#main::before {opacity: ${x}}`; 
+}
+// Sätter ett välkomstmeddelande till användaren.
+function setWelcomeMessage() {
+    document.getElementById("welcomeMessage").innerText = `Simsällskapet - Välkommen in ${localStorage.getItem("userName")}!`;
+}
+// Sparar ner data från formuläret till local storage.
+function setFormData() {
+    localStorage.setItem("userName", document.getElementById("userName").value);
+    localStorage.setItem("password", document.getElementById("password").value);
+}
+// Kollar om värdena i localstorage är samma som de korrekta login-uppgifterna. Returnerar true om sant och false om falskt.
+function checkLogin() {
+    if (localStorage.getItem("userName") === correctUserName && localStorage.getItem("password") === correctPassword) {
+        return true;
     }
     else {
-        document.getElementById("felLogIn").style.display = "flex";
-        document.getElementById("logIn").style.display = "none";
+        return false;
     }
 }
-
-forsokIgenButton.onclick = function() {
-    document.getElementById("felLogIn").style.display = "none";
-    document.getElementById("logIn").style.display = "flex";
+// Sparar data från inloggnings-formuläret. Kollar om inloggningsuppgifterna är korrekta. I så fall byter till main-page, tar bort genomskinlighet på bakgrunden och sätter välkomstmeddelande. Annars byter till försök-igen-sidan.
+function logIn() {
+    setFormData();
+    if (checkLogin()) {
+        switchToMainPage();
+        changeOpacity(1);
+        setWelcomeMessage();
+    }
+    else {
+        switchToTryAgainPage();
+    }
 }
-
-logOutButton.onclick = function() {
+// Rensar localstorage, byter till login-sidan och ökar bakgrundens genomskinlighet.
+function logOut() {
     localStorage.clear();
-    loggaUt();
+    switchToLoginPage();
+    changeOpacity(0.85);
+}
+// Kollar om användaren redan är inloggad och skickar den till huvudsidan direkt isf, annars skickas till login-sidan
+function alreadyLoggedIn() {
+    if (checkLogin()) {
+        switchToMainPage();
+        changeOpacity(1);
+        setWelcomeMessage();
+    }
+    else {
+        switchToLoginPage();
+    }
+}
+// Kör funktionen för att kolla om man är inloggad så fort sidan laddas.
+alreadyLoggedIn();
+// Vid klick på login-button så körs logIn-funktionen.
+loginButton.onclick = function() {
+    logIn();
+}
+// Vid klick på try-again-button så byter man till login-sidan.
+tryAgainButton.onclick = function() {
+    switchToLoginPage();
+}
+// Vid klick på logout-button så körs logOut-funktionen.
+logOutButton.onclick = function() {
+    logOut();
 }
